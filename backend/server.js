732ea7f119e0237aac76ac91b96e3345f54ef931
MariolaNowinski <mariola.nowinski@gmail.com>
2024-10-08 15:17:23 +0200

    task-list

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import tasksRouter from "./routes/tasks.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/tasks", tasksRouter);
+
+app.listen(3000, ()=>{
+    console.log("Der Server hört auf Port 3000!");
+});
